71f1b3484ce27ca85d465ed1f93ebb4164af32d8
Robert6916 <robert.farukshin.6916@gmail.com>
2024-05-14 20:10:38 +0300

    Update Fan.js

--- JS/Fan.js
+++ JS/Fan.js
@@ -1,7 +1,7 @@
 const Observer = new IntersectionObserver((entries, observer) => {
 	entries.forEach(entry => {
 		if (entry.isIntersecting) {
-			window.location.href = "HistoryPage.html #anchor";
+			window.location.href = "https://robert6916.github.io/ACJA_in_outline/HistoryPage.html#anchor";
 		}
 	})
 },{});
